c10c8528e1c85774632fed57e5e3e5d4a51abf89
Per Flitig <per.flitig@gmail.com>
2014-04-02 13:38:43 +0200

    Made strike tests passs, refactored tests

--- test/bowling-kata_test.js
+++ test/bowling-kata_test.js
@@ -22,60 +22,53 @@ var bowling = require('../lib/bowling-kata.js');
     test.ifError(value)
 */
 
-exports.score = {
+var testScore = function(test, input, expected) {
+  test.expect(1);
+  test.equal(bowling.score(input), expected);
+  test.done();
+};
+
+exports.point = {
   'all misses returns 0': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('--------------------'), 0);
-    test.done();
+    testScore(test, '--------------------', 0);
   },
   'all misses but one': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('9-------------------'), 9);
-    test.done();
+    testScore(test, '9-------------------', 9);
   },
   'all points and misses': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('9-9-9-9-9-9-9-9-9-9-'), 90);
-    test.done();
+    testScore(test, '9-9-9-9-9-9-9-9-9-9-', 90);
   },
+};
+
+exports.spare = {
   'spare followed by miss': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('5/-------------------'), 10);
-    test.done();
+    testScore(test, '5/-------------------', 10);
   },
   'spare followed by point': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('5/5------------------'), 20);
-    test.done();
+    testScore(test, '5/5------------------', 20);
   },
   'all spares and one bonus throw': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('5/5/5/5/5/5/5/5/5/5/5'), 150);
-    test.done();
+    testScore(test, '5/5/5/5/5/5/5/5/5/5/5', 150);
   },
+};
+
+exports.strike = {
   'strike followed by miss': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('X-------------------'), 10);
-    test.done();
+    testScore(test, 'X-------------------', 10);
   },
   'strike followed by one point': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('X5------------------'), 20);
-    test.done();
+    testScore(test, 'X5------------------', 20);
   },
   'strike followed by two points': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('X55-----------------'), 30);
-    test.done();
+    testScore(test, 'X55-----------------', 30);
   },
   'strike followed by one strike': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('XX----------------'), 30);
-    test.done();
+    testScore(test, 'XX----------------', 30);
   },
   'strike followed by one strike and two points': function(test) {
-    test.expect(1);
-    test.equal(bowling.score('XX55--------------'), 55);
-    test.done();
+    testScore(test, 'XX55--------------', 55);
+  },
+  'all strikes (two bonus rounds)': function(test) {
+    testScore(test, 'XXXXXXXXXXXX', 300);
   },
 };
