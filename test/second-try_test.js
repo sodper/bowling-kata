8261beb1feae51feb49252236e8c941c9bcefaec
Per Flitig <per.flitig@gmail.com>
2014-04-05 15:19:25 +0200

    Added a couple of more tests, begun the new design

--- test/second-try_test.js
+++ test/second-try_test.js
@@ -22,17 +22,45 @@ var bowling = require('../lib/second-try.js');
     test.ifError(value)
 */
 
+var game;
+
+var rollMany = function(n, pins) {
+  for (var i = 0; i < n; i++) {
+      game.roll(pins);
+  }
+};
+
+exports.setUp = function(callback) {
+  game = new bowling.Game();
+  callback();
+}
+
 exports.score = {
   'all misses': function(test) {
     test.expect(1);
 
-    var game = new bowling.Game();
-
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollMany(20, 0);
 
     test.equal(game.score(), 0);
     test.done();
-  }
+  },
+  'one pin on each roll': function(test) {
+    test.expect(1);
+
+    rollMany(20, 1);
+
+    test.equal(game.score(), 20);
+    test.done();
+  },
+  'one spare': function(test) {
+    test.expect(1);
+
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(17, 0);
+
+    test.equal(game.score(), 16);
+    test.done();
+  },
 };
